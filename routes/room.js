f0281e163d877cb61c32fbaca15391dca662f613
Dwi Ariyanti <dwiariyanti121@gmail.com>
2017-07-04 13:45:46 +0700

    get loc by siteid

--- routes/room.js
+++ routes/room.js
@@ -404,5 +404,249 @@ router.get('/room/getsite/',function(req,res)
             }
         }
     })
-})
+});
+router.get('/room/getloc/:_id',function(req,res)
+{
+ var siteid = req.params._id;
+ roomdb.get('room',function(err,rooms)
+    {
+        if(err)
+        {
+            if(err.message == "Key not found in database" )
+            {
+                res.json({"success": true , "obj": []});
+            }
+            else
+            {
+                res.json(500,err);
+            }
+        }
+        else
+        {
+            var listarea = [];
+            var listresult = [];
+            //get area
+            for(var i = 0 ; i < rooms.length; i++)
+            {
+                if(rooms[i].disable == false)
+                {
+                    if(rooms[i].parent == siteid)
+                    {
+                    listresult.push( rooms[i]);
+                    rooms[i].Building = [];
+                    listarea.push(rooms[i]);
+                    }
+                }
+            }
+        if(listarea.length == 0)
+        {
+                res.json({"success": true , "obj": []});
+        }
+        else
+        {
+        for(var a = 0 ; a < listarea.length;a++)
+        {
+            for(var i = 0 ; i < listarea.length;i++)
+            {
+              
+                if(listarea[i].parent != 0)
+                {
+                    if(listarea[i].parent == listarea[a].uuid)
+                    {
+                        listarea[i].parentname = listarea[a].name;
+                    }
+                }
+                else
+                {
+                     listarea[i].parentname = "-";
+                }
+            }
+        }
+             
+        for(var j = 0 ; j < rooms.length; j++)
+        {
+            for(var i = 0 ; i < listarea.length; i++)
+            {
+                if(rooms[j].disable == false)
+                {
+                    if(rooms[j].parent != 0)
+                    {
+                        if(rooms[j].parent == listarea[i].uuid)
+                        {
+                            listresult.push(rooms[j]);
+                            rooms[j].Floor = [];
+                            listarea[i].Building.push(rooms[j]);
+                        }
+                    }
+                }
+            }
+        }
+
+        for(var j = 0 ; j < rooms.length; j++)
+        {
+            for(var i = 0 ; i < listarea.length; i++)
+            {
+                for(var k = 0 ; k < listarea[i].Building.length;k++)
+                {
+                if(rooms[j].disable == false)
+                {
+                    if(rooms[j].parent != 0)
+                    {
+                       if(rooms[j].parent == listarea[i].Building[k].uuid)
+                       {
+                           listresult.push(rooms[j]);
+                           rooms[j].Zone = [];
+                           listarea[i].Building[k].Floor.push(rooms[j]);
+                       }
+                    }
+                }
+                }
+            }
+        }
+
+        for(var j = 0 ; j < rooms.length; j++)
+        {
+            for(var i = 0 ; i < listarea.length; i++)
+            {
+                for(var k = 0 ; k < listarea[i].Building.length;k++)
+                {
+                    for(var l = 0 ; l < listarea[i].Building[k].Floor.length; l++)
+                    {
+                    if(rooms[j].disable == false)
+                    {
+                        if(rooms[j].parent != 0)
+                        {
+                        if(rooms[j].parent == listarea[i].Building[k].Floor[l].uuid)
+                        {
+                            listresult.push(rooms[j]);
+                            rooms[j].Room = [];
+                            listarea[i].Building[k].Floor[l].Zone.push(rooms[j]);
+                        }
+                        }
+                    }
+                    }
+                }
+            }
+        }
+
+        for(var j = 0 ; j < rooms.length; j++)
+        {
+            for(var i = 0 ; i < listarea.length; i++)
+            {
+                for(var k = 0 ; k < listarea[i].Building.length;k++)
+                {
+                    for(var l = 0 ; l < listarea[i].Building[k].Floor.length; l++)
+                    {
+                        for(var m = 0 ; m < listarea[i].Building[k].Floor[l].Zone.length;m++)
+                        {
+                            if(rooms[j].disable == false)
+                            {
+                                if(rooms[j].parent != 0)
+                                {
+                                if(rooms[j].parent == listarea[i].Building[k].Floor[l].Zone[m].uuid)
+                                {
+                                    listresult.push(rooms[j]);
+                                    rooms[j].Section = [];
+                                    listarea[i].Building[k].Floor[l].Zone[m].Room.push(rooms[j]);
+                                }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        for(var j = 0 ; j < rooms.length; j++)
+        {
+            for(var i = 0 ; i < listarea.length; i++)
+            {
+                for(var k = 0 ; k < listarea[i].Building.length;k++)
+                {
+                    for(var l = 0 ; l < listarea[i].Building[k].Floor.length; l++)
+                    {
+                        for(var m = 0 ; m < listarea[i].Building[k].Floor[l].Zone.length;m++)
+                        {
+                            for(var n = 0 ; n < listarea[i].Building[k].Floor[l].Zone[m].Room.length; n++)
+                            {
+                            if(rooms[j].disable == false)
+                            {
+                                if(rooms[j].parent != 0)
+                                {
+                                if(rooms[j].parent == listarea[i].Building[k].Floor[l].Zone[m].Room[n].uuid)
+                                {
+                                    listresult.push(rooms[j]);
+                                    rooms[j].Closet = [];
+                                    listarea[i].Building[k].Floor[l].Zone[m].Room[n].Section.push(rooms[j]);
+                                }
+                                }
+                            }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        
+        for(var j = 0 ; j < rooms.length; j++)
+        {
+            for(var i = 0 ; i < listarea.length; i++)
+            {
+                for(var k = 0 ; k < listarea[i].Building.length;k++)
+                {
+                    for(var l = 0 ; l < listarea[i].Building[k].Floor.length; l++)
+                    {
+                        for(var m = 0 ; m < listarea[i].Building[k].Floor[l].Zone.length;m++)
+                        {
+                            for(var n = 0 ; n < listarea[i].Building[k].Floor[l].Zone[m].Room.length; n++)
+                            {
+                                for(var o = 0 ; o < listarea[i].Building[k].Floor[l].Zone[m].Room[n].Section.length; o++)
+                                {
+                                if(rooms[j].disable == false)
+                                {
+                                    if(rooms[j].parent != 0)
+                                    {
+                                    if(rooms[j].parent == listarea[i].Building[k].Floor[l].Zone[m].Room[n].Section[o].uuid)
+                                    {
+                                        listresult.push(rooms[j]);
+                                        listarea[i].Building[k].Floor[l].Zone[m].Room[n].Section[o].Closet.push(rooms[j]);
+                                    }
+                                    }
+                                }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        var result = [];
+        for(var i = 0 ; i < listresult.length; i++)
+        {
+            var selected = {
+                uuid : listresult[i].uuid,
+                name:  listresult[i].name,
+                parent : listresult[i].parent,
+                datecreated :  listresult[i].datacreated,
+                datemodified : listresult[i].datemodified,
+                changeby :  listresult[i].changeby,
+                changebyname :listresult[i].changebyname,
+                areatype :  listresult[i].areatype,
+                shortaddress: listresult[i].shortaddress,
+                fulladdress: listresult[i].fulladdress,
+                Location : listresult[i].Location,
+                disable :  listresult[i].disable
+
+            }
+            result.push(selected);
+        }
+        res.json({"success": true , "obj": result});
+        }
+           
+        }
+    });
+});
+
+
 module.exports = router;
