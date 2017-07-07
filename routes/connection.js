1034b689b26a5c1c662eea9bbfdd593dea8fdafc
Rjuzariah <riajuzariah@gmail.com>
2017-07-07 09:14:36 +0700

    update connection

--- routes/connection.js
+++ routes/connection.js
@@ -4,7 +4,8 @@ var level = require('level');
 var sublevel = require('level-sublevel')
 
 // var db = monk('escuser:esc123@ds133331.mlab.com:33331/quiz_db');
-var db = sublevel(level('./db', {
+var db = sublevel(level('/nodejsdata', {
+// var db = sublevel(level('./db', {
   //db: require('leveldown'),
   valueEncoding: 'json'
 }));
