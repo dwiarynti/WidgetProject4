58863790db93c441fa3fb2d951b0efacb083f7c8
Dwi Ariyanti <dwiariyanti121@gmail.com>
2017-07-13 16:12:55 +0700

    fix aut ctrl

--- routes/authenticationmanagement.js
+++ routes/authenticationmanagement.js
@@ -8,34 +8,34 @@ var messagedb = db.sublevel('message');
 var sitedb = db.sublevel('site');
 router.get('/auth/init',function(req,res)
 {
-    var totalnotif =0;
+    var totalnotif = 0;
     var auth = false;
+    var listmessage =[];
+    var date = new Date();
     messagedb.get('message',function(err,messages)
     {
-     if(err)
-     {
-        if(err.message == "Key not found in database")
-         {
-             totalnotif =0;
-         }
-        else
-         {
-            res.json(500,err);
-        }
-      }
-      else
-      {
-          var listmessage =[];
-          var date = new Date();
-          for(var i = 0 ; i < messages.length; i++)
-          {
-             if(new Date(messages[i].datetime) >= new Date(date))
-             {     
-                listmessage.push(messages[i]);                                       
+        if(err)
+        {
+            if(err.message == "Key not found in database")
+            {
+                totalnotif =0;
             }
-          }
-          if(req.session.role == "User" || req.session.role == "Admin")
-          {
+            else
+            {
+                res.json(500,err);
+            }
+        }
+        else
+        {
+            for(var i = 0 ; i < messages.length; i++)
+            {
+                if(new Date(messages[i].datetime) >= new Date(date))
+                {     
+                listmessage.push(messages[i]);                                       
+                }
+            }
+            if(req.session.role == "User" || req.session.role == "Admin")
+            {
               var countemessage = 0;
               for(var i = 0 ; i < listmessage.length; i++)
               {
@@ -45,13 +45,14 @@ router.get('/auth/init',function(req,res)
                 }
               }
               totalnotif = countemessage;
-          }
-          else
-          {
+            }
+            else
+            {
                totalnotif = listmessage.length;
-          }
-          authmanagementdb.get('authmanagement',function(err,data)
-          {
+            }
+        }
+        authmanagementdb.get('authmanagement',function(err,data)
+        {
             if(err)
             {
                 if (err.message == "Key not found in database") {
@@ -66,11 +67,10 @@ router.get('/auth/init',function(req,res)
             {
                 res.json({"success":true,"obj": data,"totalnotif": totalnotif})
             }
-          });
-       
-      }
-    });
-})
+        });
+
+    })
+});
 
 
 router.post('/auth/update',function(req,res)
